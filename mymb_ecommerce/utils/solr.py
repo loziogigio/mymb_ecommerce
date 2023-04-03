021c8a56aca3c45d89d083419b0f746b8d2eb421
jire87 <y.abia@crowdechain.com>
2023-04-03 03:04:32 +0000

    facet category and price update

--- mymb_ecommerce/utils/solr.py
+++ mymb_ecommerce/utils/solr.py
@@ -1,12 +1,12 @@
 import pysolr
 from urllib.parse import quote
-
+import re
 
 class Solr:
     def __init__(self, url, **kwargs):
         self.solr = pysolr.Solr(url, **kwargs)
 
-    def search(self, q, start=0, rows=10, sort=None, groups=None, **kwargs):
+    def search(self, q, start=0, rows=10, sort=None, groups=None, features=None, **kwargs):
         """
         Execute a Solr search query and return the results.
 
@@ -35,7 +35,7 @@ class Solr:
             params['facet.field'] = f'group_{len(group_list)+1}'
             params['facet.mincount'] = 1
 
-            fq_list = [f'group_{i+1}:{group.replace("-", " ")}' for i, group in enumerate(group_list)]
+            fq_list = [f'group_{i+1}:"{group.replace("-", " ")}"' for i, group in enumerate(group_list)]
             params['fq'] = fq_list
         else:
             params['facet'] = 'on'
@@ -51,12 +51,7 @@ class Solr:
         results = [dict(result) for result in response]
 
          # Get the facet counts
-        facet_counts = {}
-        if 'facet_counts' in response.raw_response:
-            for field, counts in response.raw_response['facet_counts']['facet_fields'].items():
-                if field.startswith("group"):
-                    field = "category"
-                facet_counts[field] = [{'label': label, 'count': count, 'category_url':  f"{groups},{label.replace(' ', '-')}" if groups else label} for label, count in zip(counts[::2], counts[1::2])]
+        facet_counts = Solr.get_facet(response=response,groups=groups, features=features)
 
 
         # Return the results and facet counts as a dictionary
@@ -66,4 +61,97 @@ class Solr:
             'hits': response.hits,
             'response': response
         }
+    
+    @staticmethod
+    def get_feature_suffix(feature_type):
+        """
+        Helper function to accept the feature type and return the appropriate suffix.
+        Supported feature types are: 'int', 'float', and 'string'.
+        Returns None if the feature type is not recognized.
+        """
+        if feature_type == 'int':
+            return '_feature_i'
+        elif feature_type == 'float':
+            return '_feature_f'
+        elif feature_type == 'string':
+            return '_feature_s'
+        else:
+            return None
 
+
+    @staticmethod
+    def get_facet(response, groups, features):
+        # Get the facet counts
+        facet_counts = {
+            "categories": [],
+            "features": []
+        }
+        if 'facet_counts' in response.raw_response:
+            for field, counts in response.raw_response['facet_counts']['facet_fields'].items():
+                if field.startswith("group"):
+                    # Use "categories" as the field name for group fields
+                    field = "categories"
+                    facet_counts[field] = [{'label': label, 'count': count, "url": f"{groups},{label.replace(' ', '-')}" if groups else label.replace(' ', '-')} for label, count in zip(counts[::2], counts[1::2])]
+                else:
+                    # Extract the facet name from the field name for feature fields
+                    match = re.match(r'(.*)_feature_(\w)', field)
+                    if match:
+                        clean_field = match.group(1).replace(" ", "-")
+                        label_field = match.group(1)
+                        feature_type = match.group(2)
+
+                        facet_results = [{"value": value, "count": count} for value, count in zip(counts[::2], counts[1::2])]
+
+                        facet_counts["features"].append({
+                            "clean_field": clean_field,
+                            "label_field": label_field,
+                            "type": feature_type,
+                            "facet_results": facet_results
+                        })
+                    # For other fields, use the original field name
+                    else:
+                        pass
+
+        return facet_counts
+
+    
+    @staticmethod
+    def clean_feature_name(feature_name):
+        """
+        Helper function to clean a feature name and escape any special characters for Solr.
+        Removes leading and trailing whitespace, tabs, and newlines.
+        Replaces any remaining whitespace with a single space.
+        Replaces any commas with a space.
+        Escapes any special characters with a backslash to ensure Solr can interpret them correctly.
+        Returns the cleaned and escaped feature name.
+        """
+        # Remove leading and trailing whitespace, tabs, and newlines
+        cleaned_name = feature_name.strip()
+        
+        # Replace any remaining whitespace with a single space
+        cleaned_name = re.sub(r'\s+', ' ', cleaned_name)
+        
+        # Replace any commas with a space
+        cleaned_name = cleaned_name.replace(",", " ")
+
+        # Escape any special characters for Solr
+        cleaned_name = re.sub(r'([\[\]!"#$%&\'()*+,\-./:;<=>?@\^_`{|}~])', r'\\\1', cleaned_name)
+
+        return cleaned_name
+    
+
+    @staticmethod
+    def get_feature_suffix(feature_type):
+        """
+        Helper function to accept the feature type and return the appropriate suffix.
+        Supported feature types are: 'int', 'float', and 'string'.
+        Returns None if the feature type is not recognized.
+        """
+        if feature_type == 'int':
+            return '_feature_i'
+        elif feature_type == 'float':
+            return '_feature_f'
+        elif feature_type == 'string':
+            return '_feature_s'
+        else:
+            return None
