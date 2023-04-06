d0597e795d572e564b118ccfe9c1e3c499189eb5
jire87 <y.abia@crowdechain.com>
2023-04-06 12:40:50 +0000

    dynamic db file

--- mymb_ecommerce/mymb_b2c/settings/configurations.py
+++ mymb_ecommerce/mymb_b2c/settings/configurations.py
@@ -2,6 +2,8 @@ import frappe
 from frappe import _
 from mymb_ecommerce.utils.solr import Solr
 from mymb_ecommerce.utils.database import Database
+from frappe.utils.password import get_decrypted_password
+
 
 class Configurations:
     def __init__(self):
@@ -12,6 +14,7 @@ class Configurations:
         self.mymb_b2c_payment_success_page =  self.doc.get('mymb_b2c_payment_success_page')
         self.mymb_b2c_payment_failed_page=  self.doc.get('mymb_b2c_payment_failed_page')
         self.solr = None
+        
         #mysql section
         # self.mysql_settings = self.doc.get('mysql_settings').as_dict()
         # self.mysql_connection = None
@@ -33,16 +36,27 @@ class Configurations:
         """Get Success payment page from the Mymb b2c Settings DocType"""
 
         return self.mymb_b2c_payment_success_page
-
-    def get_mymb_b2c_payment_failed_page(self):
-        """Get Failed payment page from the Mymb b2c Settings DocType"""
-
-        return self.mymb_b2c_payment_failed_page
     
-    # def get_mysql_connection(self):
-    #     """Get the MySQL connection from the Mymb b2c Settings DocType"""
 
-    #     if not self.mysql_connection:
-    #         self.mysql_connection = Database(self.mysql_settings)
+    def get_mysql_connection(self):
+        """Get the MySQL connection from the Mymb b2c Settings DocType"""
+        if not hasattr(self, 'mysql_connection'):
+            username = self.doc.get('db_username')
+            encrypted_db_password = self.doc.get('db_password')
+            db_password = get_decrypted_password("Mymb b2c Settings", self.doc.name, 'db_password')  # Decrypt the password
+            db_host = self.doc.get('db_host')
+            db_port = self.doc.get('db_port')
+            db_item_data = self.doc.get('db_item_data')
 
-    #     return self.mysql_connection
+
+            db_config = {
+                'drivername': 'mysql',
+                'username': username,
+                'password': db_password,
+                'host': db_host,
+                'port': db_port,
+                'database': db_item_data
+            }
+            self.mysql_connection = Database(db_config)
+
+        return self.mysql_connection
