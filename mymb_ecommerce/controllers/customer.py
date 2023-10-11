173157bf54470b29c781ca9937a1a527ccc472a8
jire87 <y.abia@crowdechain.com>
2023-10-11 08:49:48 +0000

    update settings autocomplete

--- mymb_ecommerce/controllers/customer.py
+++ mymb_ecommerce/controllers/customer.py
@@ -6,19 +6,17 @@ from frappe import _
 
 
 @frappe.whitelist(allow_guest=True, methods=['POST'])
-def set_search_settings(customer_code=None, customer_address_code=None, disable_gross_price_view=None):
+def set_search_settings(customer_code=None, customer_address_code=None, disable_gross_price_view=None , disable_autocomplete=None):
     # Ensure the required parameters are provided
-
-    if not customer_code or not customer_address_code or disable_gross_price_view not in [True, False]:
+    if not customer_code or not customer_address_code or disable_gross_price_view not in [True, False] or disable_autocomplete not in [True, False]:
         return {
-            "data": _("Customer code, Customer address code, and disable gross price view are mandatory.")
+            "data": _("Customer code, Customer address code, disable_gross_price_view, disable_autocomplete are mandatory.")
         }
-
 
     # Fetch the existing record, if any
     existing_record = frappe.get_value("Customer Search Settings",
                                        {"customer_code": customer_code, "customer_address_code": customer_address_code},
-                                       ['name', 'customer_code','customer_address_code','disable_gross_price_view','admin_disable_gross_price_view'],
+                                       ['name', 'customer_code','customer_address_code','disable_gross_price_view','admin_disable_gross_price_view', 'disable_autocomplete'],
                                        as_dict=True)
 
     # If the record doesn't exist, create a new one
@@ -27,7 +25,8 @@ def set_search_settings(customer_code=None, customer_address_code=None, disable_
             "doctype": "Customer Search Settings",
             "customer_code": customer_code,
             "customer_address_code": customer_address_code,
-            "disable_gross_price_view": disable_gross_price_view
+            "disable_gross_price_view": disable_gross_price_view,
+            "disable_autocomplete": disable_autocomplete
         })
         new_record.insert(ignore_permissions=True)
         frappe.db.commit()
@@ -36,7 +35,10 @@ def set_search_settings(customer_code=None, customer_address_code=None, disable_
     # If the record exists and admin_disable_gross_price_view is not True, update it
     else:
         if not existing_record.admin_disable_gross_price_view:
-            frappe.db.set_value("Customer Search Settings", existing_record.name, "disable_gross_price_view", disable_gross_price_view)
+            frappe.db.set_value("Customer Search Settings", existing_record.name, {
+                "disable_gross_price_view": disable_gross_price_view,
+                "disable_autocomplete": disable_autocomplete
+            })
             frappe.db.commit()
             data = _("Settings updated successfully!")
         else:
@@ -45,6 +47,7 @@ def set_search_settings(customer_code=None, customer_address_code=None, disable_
     return {
         "data": data
     }
+
 
 
 
@@ -61,7 +64,7 @@ def get_search_settings(customer_code, customer_address_code):
     # Fetch the existing record
     existing_record = frappe.get_value("Customer Search Settings",
                                        {"customer_code": customer_code, "customer_address_code": customer_address_code},
-                                       ['name', 'customer_code','customer_address_code','disable_gross_price_view','admin_disable_gross_price_view'],
+                                       ['name', 'customer_code','customer_address_code','disable_gross_price_view','admin_disable_gross_price_view' , 'disable_autocomplete'],
                                        as_dict=True)
     
     # Return the existing record if found, or default values
@@ -74,7 +77,8 @@ def get_search_settings(customer_code, customer_address_code):
                 "customer_code": customer_code,  # Use provided input value
                 "customer_address_code": customer_address_code,  # Use provided input value
                 "disable_gross_price_view": disable_gross_price_view,
-                "admin_disable_gross_price_view": admin_disable_gross_price_view
+                "admin_disable_gross_price_view": admin_disable_gross_price_view,
+                "disable_autocomplete":  existing_record.get("disable_autocomplete")
             }
         }
     else:
@@ -82,7 +86,8 @@ def get_search_settings(customer_code, customer_address_code):
             "customer_code": customer_code,
             "customer_address_code": customer_address_code,
             "disable_gross_price_view": False,
-            "admin_disable_gross_price_view": False
+            "admin_disable_gross_price_view": False,
+            "disable_autocomplete":  False
         }
         return {
             "message": {
